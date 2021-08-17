0ee8ffd465889f6b9f14ed0d3de226a083c39473
Bobby Lerias <bobbylerias@yahoo.com>
2021-08-17 22:52:43 +0000

    Added CPU-Strassen-Dagee into run.sh
    Co-authored-by: Kiran Ranganath <kranganath@ece.ucr.edu>

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 if [ $# -eq 0 ]
 then
-  echo "$0 [--gpu | --cpu | --gpu-dagee]"
+  echo "$0 [--gpu | --cpu | --gpu-dagee | --cpu-dagee]"
   exit
 fi
 
@@ -17,8 +17,11 @@ then
 elif [ $1 = "--gpu-dagee" ]
 then
   cmd="GPU_Strassen_dagee"
+elif [ $1 = "--cpu-dagee" ]
+then
+  cmd="CPU_Strassen_dagee"
 else
-  echo "$0 [--gpu | --cpu | --gpu-dagee]"
+  echo "$0 [--gpu | --cpu | --gpu-dagee | --cpu-dagee]"
   exit
 fi
 
