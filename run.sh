6af79ddaf53c8756055f0eed51ec837236aafb16
Bobby Lerias <bobbylerias@yahoo.com>
2021-08-18 00:13:00 +0000

    fixed run.sh errors

--- run.sh
+++ run.sh
@@ -19,13 +19,13 @@ then
   cmd="GPU_Strassen_dagee"
 elif [ $1 = "--cpu-dagee" ]
 then
-  cmd="CPU_naive"
+  cmd="CPU_Strassen_dagee"
 elif [ $1 = "--cpu-naive" ]
 then
-  cmd="GPU_naive"
+  cmd="CPU_naive"
 elif [ $1 = "--gpu-naive" ]
 then
-  cmd="CPU_Strassen_dagee"
+  cmd="GPU_naive"
 else
   echo "$0 [--gpu | --cpu | --gpu-dagee | --cpu-dagee | --cpu-naive | --gpu-naive]"
   exit
